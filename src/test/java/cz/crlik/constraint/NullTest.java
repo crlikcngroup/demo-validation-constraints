cc6b46ad6ad2ef94cb7e634f674a1548beed60d8
Radek Crlík <nix0@centrum.cz>
2019-03-02 19:08:45 +0100

    Null test

---
+++ src/test/java/cz/crlik/constraint/NullTest.java
@@ -0,0 +1,13 @@
+package cz.crlik.constraint;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class NullTest {
+
+    @Test
+    public void testWhenNull() {
+        assertTrue(true);
+    }
+}

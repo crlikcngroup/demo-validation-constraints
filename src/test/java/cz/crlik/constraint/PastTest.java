bd77f0ec686837522174bca07fe9bbc6c95b91dd
Radek Crlík <crlik@cngroup.dk>
2019-06-30 16:07:34 +0200

    Past test

---
+++ src/test/java/cz/crlik/constraint/PastTest.java
@@ -0,0 +1,70 @@
+package cz.crlik.constraint;
+
+import org.junit.Test;
+
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+import java.time.Year;
+import java.time.temporal.ChronoUnit;
+
+public class PastTest extends AbstractTest {
+    @Test
+    public void testDateInTheFuture() {
+        Object testedObject = new Object() {
+            @Past
+            public LocalDate date = LocalDate.now().plus(1, ChronoUnit.DAYS);
+        };
+
+        shouldFail(testedObject);
+    }
+
+    @Test
+    public void testDateInThePast() {
+        Object testedObject = new Object() {
+            @Past
+            public LocalDate date = LocalDate.now().minus(1, ChronoUnit.DAYS);
+        };
+
+        shouldPass(testedObject);
+    }
+
+    @Test
+    public void testYearInTheFuture() {
+        Object testedObject = new Object() {
+            @Past
+            public Year year = Year.now().plus(5, ChronoUnit.YEARS);
+        };
+
+        shouldFail(testedObject);
+    }
+
+    @Test
+    public void testYearInThePast() {
+        Object testedObject = new Object() {
+            @Past
+            public Year year = Year.now().minus(5, ChronoUnit.YEARS);
+        };
+
+        shouldPass(testedObject);
+    }
+
+    @Test
+    public void testCurrentYear() {
+        Object testedObject = new Object() {
+            @Past
+            public Year year = Year.now();
+        };
+
+        shouldFail(testedObject);
+    }
+
+    @Test
+    public void testNull() {
+        Object testedObject = new Object() {
+            @Past
+            public LocalDate date = null;
+        };
+
+        shouldPass(testedObject);
+    }
+}
